804058405b852fa2cc52d8d7e7c0d8c3818245a0
Sunil sahu <72176852+sunilsahu000@users.noreply.github.com>
2020-10-02 10:34:15 +0530

    String International use in your program
    Thank you

--- src/com/kotlin/basic/HelloWorld.kt
+++ src/com/kotlin/basic/HelloWorld.kt
@@ -3,10 +3,7 @@ package com.kotlin.basic
 fun main(s: Array<String>) {
     println("Hello World")
 
-    var str1 = "Hello World"
-    println(str1)
-
-    var str2 : String
-    str2 = "Hello World"
-    println(str2)
-}
+    val string1 = "chankibiswas"
+    println("hello mr $string1")
+    }
+    
